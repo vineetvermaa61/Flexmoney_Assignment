6f11ae17f745c4ca26632469a199d01c55ab6268
VINEET KUMAR VERMA <103252793+vineetvermaa61@users.noreply.github.com>
2025-02-05 01:53:40 +0530

    root route adding

--- server/index.js
+++ server/index.js
@@ -17,6 +17,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
@@ -66,6 +72,7 @@ async function checkCurrentMonthEnrollment(userId) {
 }
 
 // --------------------- ROUTES ---------------------
+
 
 // Signup Route
 app.post('/signup', async (req, res) => {
